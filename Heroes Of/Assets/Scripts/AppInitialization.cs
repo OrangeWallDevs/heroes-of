37258a65c0692a9a5df01490d791bcdf8bb5ff3f
Daniel Franco Lopes <dandanielfranco@yahoo.com.br>
2019-07-30 17:03:51 -0300

    Set Play Games Services Initialization

---
+++ Heroes Of/Assets/Scripts/AppInitialization.cs
@@ -0,0 +1,20 @@
+﻿using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using UnityEngine;
+
+public class AppInitialization : MonoBehaviour {
+    
+    void Awake() {
+        // Create client configuration
+        PlayGamesClientConfiguration config =
+                new PlayGamesClientConfiguration.Builder().Build();
+
+        // Enable debugging output
+        PlayGamesPlatform.DebugLogEnabled = true;
+        
+        // Initialize and activate the platform
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate(); 
+    }
+
+}
