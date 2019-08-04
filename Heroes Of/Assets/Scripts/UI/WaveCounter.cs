923ee4e0d13ab180965db2a2ab966af0f4d2292b
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-08-04 09:43:26 -0300

    Create a class to hold the references of the wave counter components

---
+++ Heroes Of/Assets/Scripts/UI/WaveCounter.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveCounter : MonoBehaviour {
+
+    private GameObject counter;
+    private TextMeshProUGUI number;
+    private Image image;
+
+    private void Start() {
+
+        number = GetComponentInChildren<TextMeshProUGUI>();
+        image = GetComponent<Image>();
+        counter = gameObject;
+
+    }
+
+    public GameObject Counter { get { return counter; } }
+
+    public TextMeshProUGUI Number { get { return number; } }
+
+    public Image Image { get { return image; } }
+
+}
