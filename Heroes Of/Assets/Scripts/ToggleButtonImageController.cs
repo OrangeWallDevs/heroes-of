b061e6bfa16df2866ecc35ce9171f6057685a92f
Pedro Paulo <ppagbpedro@gmail.com>
2019-08-03 22:45:48 -0300

    Adding script to control the sound button image
    Based on 'music' category state (muted/not muted)

---
+++ Heroes Of/Assets/Scripts/ToggleButtonImageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ToggleButtonImageController : MonoBehaviour {
+   public Button button;
+   public Sprite soundOn;
+   public Sprite soundOff;
+   public UnityAction<bool> action;
+
+   void Start() {
+       action += ToggleBackgroundImage;
+   }
+   
+   void ToggleBackgroundImage(bool IsMuted) {
+       if(IsMuted) {
+           button.GetComponent<Image>().sprite = soundOff;
+       } else {
+           button.GetComponent<Image>().sprite = soundOn;
+       }
+   }
+}
