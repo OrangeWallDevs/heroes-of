d38e49ce8115e6bc388ab20b526e1edaad92a2a6
Pedro Paulo <ppagbpedro@gmail.com>
2019-08-03 21:24:15 -0300

    Creating basic script to toggle mute when button is pressed

---
+++ Heroes Of/Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour { 
+    public AudioCategory referenceCategory;
+    public AudioData song;
+    public Button soundToggleBtn;
+
+    void Start() {
+        song.Play();
+    }
+
+    public void Mute() {
+        referenceCategory.ToggleMute();
+    }
+
+}
