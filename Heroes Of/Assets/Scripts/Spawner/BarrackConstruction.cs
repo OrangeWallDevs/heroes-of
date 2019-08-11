e65895a6a3ab4b088c23d9b4776ff9c0db5a8819
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-08-11 11:36:30 -0300

    Update BarrackConstruction script to get data more dynamically

--- Heroes Of/Assets/Scripts/Spawner/BarrackConstruction.cs
+++ Heroes Of/Assets/Scripts/Spawner/BarrackConstruction.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class BarrackConstruction : MonoBehaviour {
 
+    public WaveManager waveManager;
+
     private GameObject barrackPrefab;
-    private Sprite barrackSprite;
+    private List<Sprite> barrackSprites;
+
+    private int barrackID;
 
     private bool isPlaceSelected;
 
     private void Start() {
 
         barrackPrefab = (GameObject) Resources.Load("Prefabs/Barrack", typeof(GameObject));
-        barrackSprite = (Sprite) Resources.Load("Sprites/Barracks/Hunter_Barrack", typeof(Sprite));
+
+        barrackSprites = new List<Sprite>();
+        foreach (Sprite sprite in Resources.LoadAll("Sprites/Barracks", typeof(Sprite))) {
+
+            barrackSprites.Add(sprite);
+
+        }
 
     }
 
-    public void SelectBuildPosition() {
+    public void SelectBuildPosition(int barrackID) {
 
         isPlaceSelected = false;
+        this.barrackID = barrackID;
+
         StartCoroutine(BuildPositionDetection());
 
     }
@@ -33,7 +45,10 @@ public class BarrackConstruction : MonoBehaviour {
 
                 Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-                BuildBarrack(clickPosition);
+                Barrack newBarrack = BuildBarrack(clickPosition);
+                Spawner barrackSpawner = newBarrack.GameObject.GetComponent<Spawner>();
+
+                waveManager.AddSpawner(barrackSpawner);
 
                 yield break;
 
@@ -51,23 +66,78 @@ public class BarrackConstruction : MonoBehaviour {
     private Barrack BuildBarrack(Vector2 position) {
 
         GameObject barrackGameObject = Instantiate(barrackPrefab, position, new Quaternion());
-        barrackGameObject.GetComponent<SpriteRenderer>().sprite = barrackSprite;
 
-        Barrack barrack = new Barrack(barrackGameObject);
+        Barrack barrack = LoadBarrackData(barrackGameObject);
 
-        barrack.Name = "Hunter Barrack";
-        barrack.Description = "Spawn a bunch of Hunters";
-        barrack.SpawnFrequency = 1f;
-        barrack.SpawnLimit = 15;
-        barrack.MoneyValue = 25;
+        foreach (Sprite sprite in barrackSprites) {
+
+            if (sprite.name == barrack.Type + "_Barrack") {
+
+                barrack.GameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+                break;
+
+            }
+
+        }
 
         return barrack;
 
     }
 
-    public Sprite BarrackSprite {
+    private Barrack LoadBarrackData(GameObject gameObject) {
 
-        set { barrackSprite = value; }
+        Barrack barrack = new Barrack(gameObject);
+
+        switch (barrackID) {
+
+            case 1:
+                barrack.Type = "Assassin";
+                barrack.Name = "Assassins Barrack";
+                barrack.Description = "Spawn a bunch of Assassins";
+                barrack.SpawnFrequency = 1f;
+                barrack.SpawnLimit = 15;
+                barrack.MoneyValue = 25;
+                break;
+
+            case 2:
+                barrack.Type = "Hunter";
+                barrack.Name = "Hunters Barrack";
+                barrack.Description = "Spawn a bunch of Hunters";
+                barrack.SpawnFrequency = 1f;
+                barrack.SpawnLimit = 15;
+                barrack.MoneyValue = 25;
+                break;
+
+            case 3:
+                barrack.Type = "Knight";
+                barrack.Name = "Knights Barrack";
+                barrack.Description = "Spawn a bunch of Knights";
+                barrack.SpawnFrequency = 1f;
+                barrack.SpawnLimit = 15;
+                barrack.MoneyValue = 25;
+                break;
+
+            case 4:
+                barrack.Type = "Mage";
+                barrack.Name = "Mages Barrack";
+                barrack.Description = "Spawn a bunch of Mages";
+                barrack.SpawnFrequency = 1f;
+                barrack.SpawnLimit = 15;
+                barrack.MoneyValue = 25;
+                break;
+
+            case 5:
+                barrack.Type = "Soldier";
+                barrack.Name = "Soldiers Barrack";
+                barrack.Description = "Spawn a bunch of Soldiers";
+                barrack.SpawnFrequency = 1f;
+                barrack.SpawnLimit = 15;
+                barrack.MoneyValue = 25;
+                break;
+
+        }
+
+        return barrack;
 
     }
 
