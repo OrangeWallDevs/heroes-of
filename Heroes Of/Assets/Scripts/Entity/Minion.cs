b34236dcc7d67c1b4981b9006d7627b4449ecfd4
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-08-11 11:32:07 -0300

    Create a Minion Entity for testing

---
+++ Heroes Of/Assets/Scripts/Entity/Minion.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Minion : Entity {
+
+    private string name;
+
+    private int damage;
+    private int health;
+
+    private int movementSpeed;
+    private int attackSpeed;
+
+    private int killPoints;
+    private int killMoney;
+
+    public Minion(GameObject gameObject) : base (gameObject) { } 
+
+    public string Name {
+
+        get { return name; }
+
+        set { name = value; }
+
+    }
+
+    public int Damage {
+
+        get { return damage; }
+
+        set { damage = value; }
+    }
+
+    public int Health {
+
+        get { return health; }
+
+        set { health = value; }
+
+    }
+
+    public int MovementSpeed {
+
+        get { return movementSpeed; }
+
+        set { movementSpeed = value; }
+
+    }
+
+    public int AttackSpeed {
+
+        get { return attackSpeed; }
+
+        set { attackSpeed = value; }
+
+    }
+
+    public int KillPoints {
+
+        get { return killPoints; }
+
+        set { killPoints = value; }
+
+    }
+
+    public int KillMoney {
+
+        get { return killMoney; }
+
+        set { killMoney = value; }
+
+    }
+
+}
