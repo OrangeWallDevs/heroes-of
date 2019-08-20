f3453c4ffb80a57c23e068e00d7ba2dfea12e7e6
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-08-20 19:44:30 -0300

    Create a animation to hide and show the barracks panel

---
+++ Heroes Of/Assets/Scripts/UI/BarracksPanel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class BarracksPanel : MonoBehaviour { 
+
+    private bool isOpen;
+    private Animator animator;
+
+    private void Start() {
+
+        isOpen = gameObject.activeInHierarchy;
+
+    }
+
+    private void Awake() {
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    public void ToggleVisibilityBarracksMenu() {
+
+        isOpen = !isOpen;
+
+        if (isOpen) {
+
+            gameObject.SetActive(isOpen);
+            animator.Play("Open_Animation");
+
+        }
+        else {
+
+            animator.Play("Hide_Animation");
+
+        }
+
+    }
+
+    public void RemoveBarracksMenu() {
+
+        gameObject.SetActive(isOpen);
+
+    }
+
+}
