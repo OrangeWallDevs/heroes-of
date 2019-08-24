edc4b56a4f3e707f2225ca7d3d9332834047adb3
Pedro Paulo <ppagbpedro@gmail.com>
2019-08-24 17:37:36 -0300

    Creating Pause Manager script and and a prefab to assign it

---
+++ Heroes Of/Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+    public static bool IsGamePaused {get; private set;}
+    public GameObject PauseMenu;
+
+    private void Awake() {
+        IsGamePaused = false;
+    }
+
+    public void TogglePause() {
+        PauseMenu.SetActive(!PauseMenu.activeSelf);
+        IsGamePaused = !IsGamePaused;
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+
+    public void RestartGame() {
+        Debug.Log("Restarting game...");
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quiting game...");
+    }
+}
