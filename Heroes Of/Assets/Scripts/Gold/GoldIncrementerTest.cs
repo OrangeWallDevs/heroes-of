0a038ae4ca8aa562c22c63929ff622a38dcbbbba
Pedro Paulo <ppagbpedro@gmail.com>
2019-09-01 19:12:45 -0300

    Create placeholder for GameRuntimeData for test pruporses

---
+++ Heroes Of/Assets/Scripts/Gold/GoldIncrementerTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="ScriptableObject/GoldIncrementTest")]
+public class GoldIncrementerTest : ScriptableObject {
+    public PlayerGoldReserve playerGoldReserve {get; private set;}
+    public IntEvent goldChangeEvent;
+    void OnEnable() {
+        playerGoldReserve = new PlayerGoldReserve(500, null, goldChangeEvent);
+        playerGoldReserve.SpendGold(200);
+    }
+}
