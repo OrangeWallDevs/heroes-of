3fed38e7613fc2c29a8717c0426830f607a4c43a
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-01 18:55:14 -0300

    Create a basic script for the movement logic of a Troop

---
+++ Heroes Of/Assets/Scripts/Troop/TroopMovementActions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopMovementActions : MonoBehaviour {
+
+    private Rigidbody2D troopRigidBody2d;
+    private RunTimeTroopData troopData;
+    private IsometricCharacterAnimator troopAnimations;
+
+    void Start() {
+
+        troopRigidBody2d = GetComponent<Rigidbody2D>();
+        troopData = GetComponent<RunTimeTroopData>();
+        troopAnimations = GetComponentInChildren<IsometricCharacterAnimator>();
+
+    }
+
+    public void MoveToPosition(Vector2 targetPosition) {
+
+        Vector2 actualPosition = transform.position;
+
+        Vector2 movementDirection = (targetPosition - actualPosition).normalized;
+        movementDirection = Vector2.ClampMagnitude(movementDirection, 1);
+
+        Vector2 movement = movementDirection * troopData.vlrMotionSpeed;
+        Vector2 fixedTargetPosition = actualPosition + movement * Time.fixedDeltaTime;
+
+        troopRigidBody2d.MovePosition(fixedTargetPosition);
+        troopAnimations.AnimateRun(targetPosition);
+
+    }
+
+    public void WaitOnActualPosition() {
+
+        troopAnimations.AnimateStatic();
+
+    }
+
+}
