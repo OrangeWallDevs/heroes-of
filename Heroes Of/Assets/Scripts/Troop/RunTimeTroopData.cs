e58bcd2fe522c2a1377e2dcdc9c5dc1ea517c735
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-01 18:50:38 -0300

    Create a script to simulate the RunTimeData info

---
+++ Heroes Of/Assets/Scripts/Troop/RunTimeTroopData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunTimeTroopData : MonoBehaviour {
+
+    public string namTroop;
+    public int vlrDamageDealt;
+    public int vlrHp;
+    public int vlrScore;
+    public int vlrMotionSpeed;
+    public int vlrDropMoney;
+    public float vlrAttackSpeed;
+
+    public bool isEnemy;
+
+    public GameObject GameObject {
+        get { return gameObject; }
+    }
+
+}
