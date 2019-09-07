63e6663a09f6308628aa657a74415fda25a97636
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-07 11:24:45 -0300

    Create a script to dynamically resize the battle collider

---
+++ Heroes Of/Assets/Scripts/Troop/DynamicBattleColliderSize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DynamicBattleColliderSize : MonoBehaviour {
+
+    public float detectionIncreasedDistance = 0f;
+
+    private RunTimeTroopData troopData;
+    private CircleCollider2D troopBattleCollider;
+
+    void Start() {
+
+        troopBattleCollider = GetComponent<CircleCollider2D>();
+        troopData = GetComponentInParent<RunTimeTroopData>();
+
+        troopBattleCollider.radius = (troopData.attackDistance + detectionIncreasedDistance) / 10;
+        
+    }
+
+}
