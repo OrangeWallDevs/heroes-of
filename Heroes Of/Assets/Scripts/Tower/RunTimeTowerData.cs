08349cc633a0519c25451ad0183b74856d64d9eb
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-08 19:24:15 -0300

    Create the RunTimeTowerData
    class only for testing

---
+++ Heroes Of/Assets/Scripts/Tower/RunTimeTowerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunTimeTowerData : RunTimeData {
+
+    public int vlrHp;
+    public int numEffectArea;
+
+    public bool isEnemy;
+
+}
