322c10471d7d16d01e2dbc40c83af70f17905d69
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-08 19:28:39 -0300

    Create the troop implementation of HealthController

---
+++ Heroes Of/Assets/Scripts/Troop/TroopHealthController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class TroopHealthController : HealthController {
+
+    public TroopEvent troopDeathEvent;
+    public GameObject deathEffect;
+
+    private RunTimeTroopData troopData;
+
+    private void Start() {
+
+        troopData = GetComponent<RunTimeTroopData>();
+
+        Health = troopData.vlrHp;
+
+    }
+
+    public override void Die() {
+
+        if (deathEffect != null) {
+
+            Instantiate(deathEffect, transform.position, transform.rotation);
+
+        }
+
+        Destroy(gameObject);
+
+    }
+
+    public override void ReceiveDamage(int vlrDamageReceived) {
+
+        troopData.vlrHp -= vlrDamageReceived;
+        Health = troopData.vlrHp;
+
+        if (troopData.vlrHp <= 0) {
+
+            troopDeathEvent.Raise(troopData);
+            Die();
+
+        }
+
+    }
+
+}
