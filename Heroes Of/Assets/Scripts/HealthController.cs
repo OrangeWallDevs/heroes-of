515f6e44cc36c79f9f141d439e221c52b1f8fc3e
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-08 19:27:27 -0300

    Create HealthController abstract class
    this class is used to remove the responsibility from the AI of controlling the health. Is also can inform the current health of a object

---
+++ Heroes Of/Assets/Scripts/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class HealthController : MonoBehaviour {
+
+    private float health;
+
+    public abstract void ReceiveDamage(int vlrDamageReceived);
+
+    public abstract void Die();
+
+    public float Health {
+
+        get { return health; }
+        protected set { health = value; }
+
+    }
+
+}
