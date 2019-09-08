bd2ab1472c9b2b692954f74dc028315fa211ceb9
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-08 19:33:18 -0300

    Update the troop AI to interact with towers

--- Heroes Of/Assets/Scripts/Troop/TroopIA.cs
+++ Heroes Of/Assets/Scripts/Troop/TroopIA.cs
@@ -4,18 +4,66 @@ using UnityEngine;
 
 public class TroopIA : MonoBehaviour {
 
-    public Transform targetPoint;
-
     public TroopEvent troopDeathEvent;
+    public TowerEvent towerBeingAttackedEvent, towerDestroyedEvent;
 
     private TroopStates actualState;
 
     private List<RunTimeTroopData> enemysTroopsDetected;
     private GameObject actualAttackingTarget;
 
+    private List<RunTimeTowerData> towersList;
+    private RunTimeTowerData detectedEnemyTower;
+    private GameObject actualTargetTowerToWalk, lastAttackedTower;
+
     private RunTimeTroopData troopData;
     private TroopMovementActions movementAction;
     private TroopAttackActions attackAction;
+
+    private void Awake() {
+
+        GameObject[] towersInGame = GameObject.FindGameObjectsWithTag("Tower");
+        float[] distancesBetweenTower = new float[towersInGame.Length];
+
+        Vector2 troopPosition = transform.position;
+
+        for (int i = 0; i < towersInGame.Length; i++) {
+
+            Vector2 towerPosition = towersInGame[i].transform.position;
+            distancesBetweenTower[i] = Vector2.Distance(troopPosition, towerPosition);
+
+        }
+
+        for (int i = 1; i < towersInGame.Length; i++) { // Order the towers by distance
+
+            float lastComparationDistance = distancesBetweenTower[i];
+            GameObject lastComparationTower = towersInGame[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && distancesBetweenTower[j] > lastComparationDistance) {
+
+                distancesBetweenTower[j + 1] = distancesBetweenTower[j];
+                towersInGame[j + 1] = towersInGame[j];
+
+                j--;
+
+            }
+
+            distancesBetweenTower[j + 1] = lastComparationDistance;
+            towersInGame[j + 1] = lastComparationTower;
+
+        }
+
+        towersList = new List<RunTimeTowerData>();
+
+        foreach (GameObject tower in towersInGame) {
+
+            towersList.Add(tower.GetComponent<RunTimeTowerData>());
+
+        }
+
+    }
 
     private void Start() {
 
@@ -24,8 +72,9 @@ public class TroopIA : MonoBehaviour {
         troopData = GetComponent<RunTimeTroopData>();
         movementAction = GetComponent<TroopMovementActions>();
         attackAction = GetComponent<TroopAttackActions>();
-
+        
         troopDeathEvent.RegisterListener(OnTroopDeath);
+        towerDestroyedEvent.RegisterListener(OnTowerDestruction);
 
     }
 
@@ -33,40 +82,21 @@ public class TroopIA : MonoBehaviour {
 
         if (enemysTroopsDetected.Count > 0) {
 
-            RunTimeTroopData actualTargetEnemy = enemysTroopsDetected.ToArray()[0];
-            Vector2 actualTargetEnemyPosition = actualTargetEnemy.transform.position;
+            HandleEnemyTroopAttack();
 
-            if (IsInRange(actualTargetEnemyPosition, troopData.attackDistance)) { // Attack
+        } 
+        else if (towersList.Count > 0) {
 
-                if (actualTargetEnemy.gameObject != actualAttackingTarget) {
+            if (troopData.troopObjective == PhaseObjectives.DEFEND) {
 
-                    actualState = TroopStates.FIGHTING;
-                    actualAttackingTarget = actualTargetEnemy.gameObject;
-                    attackAction.AttackTroop(actualTargetEnemy);
-
-                }
+                HandleTowerDefense();
 
             }
-            else { // Get colser to the Enemy
+            else {
 
-                actualState = TroopStates.MOVING;
-                movementAction.MoveToPosition(actualTargetEnemyPosition);
+                HandleTowerAttack();
 
             }
-
-            return;
-
-        }
-
-        if (!IsInTowerPosition()) {
-
-            actualState = TroopStates.MOVING;
-            movementAction.MoveToPosition(targetPoint.position);
-
-        }
-        else {
-
-            movementAction.WaitOnActualPosition();
 
         }
 
@@ -97,7 +127,13 @@ public class TroopIA : MonoBehaviour {
 
             case ("Tower"):
 
-                Debug.Log("Attack Tower");
+                RunTimeTowerData detectedTower = detectedObject.GetComponent<RunTimeTowerData>();
+
+                if (detectedTower.isEnemy != troopData.isEnemy) {
+
+                    detectedEnemyTower = detectedTower;
+
+                }
                 break;
 
             case ("Hero"):
@@ -109,13 +145,75 @@ public class TroopIA : MonoBehaviour {
 
     }
 
-    private bool IsInTowerPosition() {
+    private void HandleEnemyTroopAttack() {
 
-        Vector2 towerPosition = targetPoint.position;
-        Vector2 actualPosition = transform.position;
+        RunTimeTroopData actualTargetEnemy = enemysTroopsDetected.ToArray()[0];
+        Vector2 actualTargetEnemyPosition = actualTargetEnemy.transform.position;
 
-        return !(Mathf.Ceil(towerPosition.x) != Mathf.Ceil(actualPosition.x) 
-            || Mathf.Ceil(towerPosition.y) != Mathf.Ceil(actualPosition.y));
+        if (IsInRange(actualTargetEnemyPosition, troopData.attackDistance)) { // Attack
+
+            if (actualTargetEnemy.gameObject != actualAttackingTarget) {
+
+                actualState = TroopStates.FIGHTING;
+                actualAttackingTarget = actualTargetEnemy.gameObject;
+                attackAction.Attack(actualTargetEnemy);
+
+            }
+
+        }
+        else { // Get colser to the Enemy
+
+            actualState = TroopStates.MOVING;
+            movementAction.MoveToPosition(actualTargetEnemyPosition);
+
+        }
+
+        return;
+
+    }
+
+    private void HandleTowerDefense() {
+
+        actualTargetTowerToWalk = FindTargetTower();
+
+        if (!IsInTowerPosition()) {
+
+            actualState = TroopStates.MOVING;
+            movementAction.MoveToPosition(actualTargetTowerToWalk.transform.position);
+
+        } 
+        else {
+
+            movementAction.WaitOnActualPosition();
+
+        }
+
+    }
+
+    private void HandleTowerAttack() {
+
+        actualTargetTowerToWalk = FindTargetTower();
+
+        if (IsInRange(actualTargetTowerToWalk.transform.position, troopData.attackDistance)) {
+
+            if (lastAttackedTower != detectedEnemyTower.GameObject) {
+                
+                actualState = TroopStates.ATTACKING;
+
+                lastAttackedTower = detectedEnemyTower.GameObject;
+                towerBeingAttackedEvent.Raise(detectedEnemyTower);
+                attackAction.Attack(detectedEnemyTower);
+
+            }
+
+        } 
+        else {
+
+            actualState = TroopStates.MOVING;
+
+            movementAction.MoveToPosition(actualTargetTowerToWalk.transform.position);
+
+        }
 
     }
 
@@ -129,23 +227,32 @@ public class TroopIA : MonoBehaviour {
 
     }
 
-    public void ReceiveDamage(int vlrDamageReceived) {
+    private GameObject FindTargetTower() {
 
-        troopData.vlrHp -= vlrDamageReceived;
+        GameObject targetTower;
 
-        if (troopData.vlrHp <= 0) {
+        if (troopData.troopObjective == PhaseObjectives.ATTACK) { // Get the closest tower as target
 
-            Die();
+            targetTower = towersList.ToArray()[0].GameObject;
+
+        } 
+        else { // Get the most distant tower as target
+
+            targetTower = towersList.ToArray()[towersList.Count - 1].GameObject;
 
         }
 
+        return targetTower;
+
     }
 
-    public void Die() {
+    private bool IsInTowerPosition() {
 
-        // Instanciate(deathEffect, transform.position, transform.rotation);
-        troopDeathEvent.Raise(troopData);
-        Destroy(gameObject);
+        Vector2 towerPosition = actualTargetTowerToWalk.transform.position;
+        Vector2 actualPosition = transform.position;
+
+        return !(Mathf.Ceil(towerPosition.x) != Mathf.Ceil(actualPosition.x)
+            || Mathf.Ceil(towerPosition.y) != Mathf.Ceil(actualPosition.y));
 
     }
 
@@ -162,6 +269,18 @@ public class TroopIA : MonoBehaviour {
             }
 
         }
+
+    }
+
+    private void OnTowerDestruction(RunTimeTowerData tower) {
+
+        if (detectedEnemyTower == tower) {
+
+            detectedEnemyTower = null;
+
+        }
+
+        towersList.Remove(tower);
 
     }
 
