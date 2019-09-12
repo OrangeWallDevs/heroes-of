990ca94d7ea8336bcd33ca4331185be5b6a26f5f
Pedro Paulo <ppagbpedro@gmail.com>
2019-09-11 21:43:35 -0300

    Create script to open and close a UI modal
    This can be useful when we're working on the conclusion and settings screen

---
+++ Heroes Of/Assets/Scripts/UI/UIModalOpener.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIModalOpener : MonoBehaviour {
+
+    public GameObject uiModal;
+    
+    public void OpenModal() {
+        uiModal.SetActive(true);
+    }
+
+    public void CloseModal() {
+        uiModal.SetActive(false);
+    }
+}
