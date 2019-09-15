8275c816cbc19c1c64f4c0157176e7aaf8b55c89
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-15 12:39:17 -0300

    Create a folder for collider related scripts

---
+++ Heroes Of/Assets/Scripts/Collider/DynamicBattleColliderSize.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DynamicBattleColliderSize : MonoBehaviour {
+
+    public float detectionIncreasedDistance = 0f;
+
+    private float attackDistance;
+    private CircleCollider2D battleCollider;
+
+    void Start() {
+
+        switch (transform.parent.tag) {
+
+            case ("Troop"):
+
+                RunTimeTroopData troopData = GetComponentInParent<RunTimeTroopData>();
+                attackDistance = troopData.attackDistance;
+                break;
+
+            case ("Hero"):
+
+                /*RunTimeHeroData heroData = GetComponentInParent<RunTimeHeroData>();
+                attackDistance = heroData.attackDistance;*/
+                break;
+
+        }
+
+        battleCollider = GetComponent<CircleCollider2D>();
+        battleCollider.radius = (attackDistance + detectionIncreasedDistance) / 10;
+        
+    }
+
+}
