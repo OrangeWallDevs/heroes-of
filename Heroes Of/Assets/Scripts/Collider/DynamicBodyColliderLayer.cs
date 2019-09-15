a2728686fde9e3a85e89ef31d196c9e1d107e5b6
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-15 12:41:15 -0300

    Create a script to set dynamically the layer of body colliders
    And also adds the new layers to the project settings. These layers only collides with them selfs.

---
+++ Heroes Of/Assets/Scripts/Collider/DynamicBodyColliderLayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class DynamicBodyColliderLayer : MonoBehaviour {
+
+    private bool isEnemy;
+
+    void Start() {
+
+        switch (transform.parent.tag) {
+
+            case ("Troop"):
+
+                RunTimeTroopData troopData = GetComponentInParent<RunTimeTroopData>();
+                isEnemy = troopData.isEnemy;
+                break;
+
+            case ("Tower"):
+
+                RunTimeTowerData towerData = GetComponentInParent<RunTimeTowerData>();
+                isEnemy = towerData.isEnemy;
+                break;
+
+            case ("Hero"):
+
+                Debug.Log("TO:DO: Create a hero RunTimeData");
+                break;
+
+        }
+
+        if (isEnemy) {
+
+            gameObject.layer = 11;
+
+        }
+        else {
+
+            gameObject.layer = 12;
+
+        }
+        
+    }
+
+}
