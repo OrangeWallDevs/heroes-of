3330fdf3613cd206db31482d5ae40f70e8d5e1e8
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-15 15:17:53 -0300

    Refactory the script to use a TemplateMethod pattern for the TroopAttack

--- Heroes Of/Assets/Scripts/TroopAttack/TroopAttackI.cs
+++ Heroes Of/Assets/Scripts/TroopAttack/TroopAttackI.cs
@@ -5,13 +5,35 @@ public abstract class TroopAttackI {
 
     protected IsometricCharacterAnimator troopAnimations;
     protected RunTimeTroopData troopData;
+    protected TroopAttackActions attackActions;
 
     public TroopAttackI(IsometricCharacterAnimator isometricCharacterAnimations, RunTimeTroopData troopData) {
 
         troopAnimations = isometricCharacterAnimations;
+        attackActions = troopData.GetComponent<TroopAttackActions>();
 
     }
 
-    public abstract IEnumerator Attack(RunTimeData entityData);
+    public Coroutine Attack(RunTimeTroopData troopData) {
+
+        GameObject enemyTroop = troopData.GameObject;
+        HealthController troopHealthController = enemyTroop.GetComponent<TroopHealthController>();
+
+        return attackActions.StartCoroutine(AttackCoroutine(enemyTroop.transform, troopHealthController));
+
+    }
+
+    public Coroutine Attack(RunTimeTowerData towerData) {
+
+        GameObject enemyTower = towerData.GameObject;
+        HealthController towerHealthController = enemyTower.GetComponent<TowerHealthController>();
+
+        return attackActions.StartCoroutine(AttackCoroutine(enemyTower.transform, towerHealthController));
+
+    }
+
+    //public Coroutine Attack(RunTimeHeroData heroData) { }
+
+    protected abstract IEnumerator AttackCoroutine(Transform enemy, HealthController enemyHealthController);
 
 }
