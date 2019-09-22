e738eeb70a71d60f874ad5e07d0bd9f8e63a99d9
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-22 11:44:10 -0300

    Create a RunTimeData for the Hero

---
+++ Heroes Of/Assets/Scripts/Hero/RunTimeHeroData.cs
@@ -0,0 +1,18 @@
+﻿public class RunTimeHeroData : RunTimeData {
+
+    public string namHero;
+    public string desHero;
+    public int vlrHp;
+    public int vlrScore;
+    public int vlrDamageDealt;
+    public int vlrDropMoney;
+
+    // Add to the entity classes and BD
+    public float vlrMotionSpeed;
+    public float vlrAttackSpeed;
+    public float vlrAttackDistance;
+    public PhaseObjectives heroObjective;
+    public bool isEnemy;
+    public bool attackAtDistance;
+
+}
