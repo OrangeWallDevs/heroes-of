746d9ab18af71d7fc9d34020fcff4696e1488e7f
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-22 11:43:42 -0300

    Create a HeroHealthController

---
+++ Heroes Of/Assets/Scripts/Hero/HeroHealthController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class HeroHealthController : HealthController {
+
+    public GameObject deathEffect;
+    public HeroEvent heroDeathEvent;
+
+    private RunTimeHeroData heroData;
+
+    private void Start() {
+
+        heroData = GetComponent<RunTimeHeroData>();
+
+        Health = heroData.vlrHp;
+
+    }
+
+    public override void ReceiveDamage(int vlrDamageReceived) {
+
+        heroData.vlrHp -= vlrDamageReceived;
+        Health = heroData.vlrHp;
+
+        if (Health <= 0) {
+
+            Die();
+
+        }
+
+    }
+
+    public override void Die() {
+
+        if (deathEffect != null) {
+
+            Instantiate(deathEffect, transform.position, transform.rotation);
+
+        }
+
+        Destroy(gameObject);
+
+    }
+
+}
