919fdbafe15b3c9c23a88c8a5e7e01eff50bb536
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-29 19:23:03 -0300

    Create a abstract class for ordenated lists

---
+++ Heroes Of/Assets/Scripts/Util/OrdenatedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class OrdenatedList<T> where T : IComparable<T> {
+
+    private int count = 0;
+
+    public abstract void Add(T data);
+
+    public abstract T Remove(T data);
+
+    public abstract T Remove(int index);
+
+    public abstract void Clear();
+
+    public abstract int IndexOf(T data);
+
+    public abstract bool Contains(T data);
+
+    public abstract T GetLast();
+
+    public abstract T GetFirst();
+
+    public abstract T Find(int index);
+
+    public abstract List<T> ToList();
+
+    public int Count {
+
+        get { return count; }
+        protected set { count = value; }
+
+    }
+
+}
