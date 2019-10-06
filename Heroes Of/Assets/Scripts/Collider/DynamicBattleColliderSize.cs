78a68314a74747d5070c3303dd90fd0214edffe8
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-10-06 11:08:40 -0300

    Fix bug of AttackDistance be greater than the BattleCollider

--- Heroes Of/Assets/Scripts/Collider/DynamicBattleColliderSize.cs
+++ Heroes Of/Assets/Scripts/Collider/DynamicBattleColliderSize.cs
@@ -26,7 +26,7 @@ public class DynamicBattleColliderSize : MonoBehaviour {
         }
 
         battleCollider = GetComponent<CircleCollider2D>();
-        battleCollider.radius = (attackDistance + detectionIncreasedDistance) / 10;
+        battleCollider.radius = ((attackDistance / 10) + detectionIncreasedDistance);
         
     }
 
