a33a7c11947202446e58e7b501fa462fef06fb4a
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-10-06 07:58:53 -0300

    Make Barracks and Troops be created by the Factorys

---
+++ Heroes Of/Assets/Scripts/Barrack/BarrackConstruction.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrackConstruction : MonoBehaviour {
+
+    public GameObject barrackPrefab;
+    public List<Sprite> barrackSprites;
+    public GoldIncrementerTest goldIncrementerTest;
+
+    public PopUpManager alertManager;
+    private int barrackID;
+
+    private bool isPlaceSelected;
+
+    private Coroutine buildPositionSelection;
+
+    //TO:DO Use RunTimeData to load and create the barrack
+    public BarrackFactory barrackFactory;
+    public bool buildForEnemy;
+    public PhaseObjectives buildForObjective; 
+
+    public void SelectBuildPosition(int barrackID) {
+
+        if (buildPositionSelection != null) {
+
+            StopCoroutine(buildPositionSelection);
+            buildPositionSelection = null;
+
+        }
+
+        isPlaceSelected = false;
+        this.barrackID = barrackID;
+        buildPositionSelection = StartCoroutine(BuildPositionDetection());
+
+    }
+
+    private IEnumerator BuildPositionDetection() {
+
+        while (!isPlaceSelected) {
+
+            if (Input.GetMouseButtonDown(0)) {
+
+                isPlaceSelected = true;
+
+                Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                BuildBarrack(clickPosition, goldIncrementerTest.playerGoldReserve);
+
+                StopCoroutine(buildPositionSelection);
+                yield break;
+
+            } 
+            else {
+
+                yield return new WaitForFixedUpdate();
+
+            }
+
+        }
+
+    }
+
+    private Barrack BuildBarrack(Vector2 position, GoldReserve goldReserve) {
+
+        Barrack barrack = barrackFactory.CreateBarrack(barrackID, buildForEnemy, buildForObjective);
+
+        if(goldReserve.SpendGold(barrack.VlrCost)) {
+
+            barrack.GameObject.transform.position = new Vector3(position.x, position.y, 0);
+            return barrack;
+            
+        } 
+        else {
+            alertManager.ShowWarningModal("Você não tem dinheiro suficiente para comprar essa caserna!");
+            Destroy(barrack.GameObject);
+        }
+
+        return null;   
+
+    }
+
+}
---
+++ Heroes Of/Assets/Scripts/Barrack/Spawner.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject troopPrefab;
+    public Transform spawnPoint;
+
+    private WaveManager waveManager;
+
+    public int SpawnLimit { get; set; } = 1;
+    public float SpawnFrequency { get; set; } = 1f;
+
+    private int countSpawnedEnemys = 0;
+    private SpawnerStates actualState = SpawnerStates.WAITING;
+    private Coroutine spawnCicle;
+
+    //TO:DO Use RunTimeData to load and create the barrack
+    private TroopFactory troopFactory;
+    private RunTimeBarrackData barrackData;
+
+    private void Awake() {
+
+        waveManager = GameObject.FindGameObjectWithTag("Wave_Manager").GetComponent<WaveManager>();
+        troopFactory = GameObject.FindGameObjectWithTag("Factory").GetComponent<TroopFactory>();
+
+    }
+
+    private void Start() {
+
+        waveManager.RegisterSpawnerAsListener(this);
+
+        //TO:DO use RunTimeData to get the Barrack class to this respctive GO
+        barrackData = GetComponent<RunTimeBarrackData>();
+
+    }
+
+    public void StartSpawnCicle() {
+
+        actualState = SpawnerStates.SPAWNING;
+        spawnCicle = StartCoroutine(SpawnCicle());
+
+    }
+
+    public void StopSpawnCicle() {
+
+        actualState = SpawnerStates.WAITING;
+        StopCoroutine(spawnCicle);
+        spawnCicle = null;
+        
+    }
+
+    private IEnumerator SpawnCicle() {
+
+        for (countSpawnedEnemys = 1; countSpawnedEnemys < SpawnLimit; countSpawnedEnemys++) {
+
+            if (!actualState.Equals(SpawnerStates.WAITING)) {
+
+                SpawnEnemy();
+                yield return new WaitForSeconds(SpawnFrequency);
+
+            }
+            else {
+
+                break;
+
+            }
+
+        }
+
+        StopSpawnCicle();
+
+    }
+
+    private void SpawnEnemy() {
+
+        Troop troop = troopFactory.CreateTroop(barrackData.codTroop, barrackData.isEnemy, barrackData.objective);
+        troop.GameObject.transform.position = spawnPoint.position;
+
+    }
+
+}
