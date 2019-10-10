10527b3f830a101da445de8ddf89baf775e0642d
Daniel Franco Lopes <dandanielfranco@yahoo.com.br>
2019-10-09 21:49:51 -0300

    Create AssetFilter entity class

---
+++ Heroes Of/Assets/Scripts/Entity/AssetFilter.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class AssetFilter : Entity {
+    
+    string namTable;
+    string txtAssetFilter;
+    string txtAssetPath;
+
+    public string NamTable { get => namTable; set => namTable = value; }
+    public string TxtAssetFilter { get => txtAssetFilter; set => txtAssetFilter = value; }
+    public string TxtAssetPath { get => txtAssetPath; set => txtAssetPath = value; }
+    
+    public AssetFilter(GameObject gameObject) : base(gameObject) {
+
+    }
+    
+}
