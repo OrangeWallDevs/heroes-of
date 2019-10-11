43cce01c8433f327f8d4bd736fc9d7039a81493c
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-10-11 20:54:19 -0300

    Create a Core HealthController

---
+++ Heroes Of/Assets/Scripts/Core/CoreHealthController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreHealthController : HealthController {
+
+    public GameEvent coreDestroyedEvent;
+    public GameObject destructionEffectPrefab;
+
+    private RunTimeCoreData coreData;
+    private bool wasDestroyed;
+
+    private void Start() {
+
+        coreData = GetComponent<RunTimeCoreData>();
+        wasDestroyed = false;
+
+        Health = coreData.valHp;
+
+    }
+
+    public override void Die() {
+
+        if (destructionEffectPrefab != null) {
+
+            Instantiate(destructionEffectPrefab, transform.position, transform.rotation);
+
+        }
+
+        Destroy(gameObject);
+
+    }
+
+    public override void ReceiveDamage(int valDamageReceived) {
+
+        coreData.valHp -= valDamageReceived;
+        Health = coreData.valHp;
+
+        if (!wasDestroyed && Health <= 0) {
+
+            wasDestroyed = true;
+            coreDestroyedEvent.Raise();
+            Die();
+
+        }
+
+    }
+
+}
