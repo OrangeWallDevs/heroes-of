7c07a8587ca3145b7ce7520679a0ff58cac72df7
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-10-11 21:06:53 -0300

    Create the basic WinConditionChecker module of the Phase_Manager

---
+++ Heroes Of/Assets/Scripts/Phase/WinConditionObserver.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinConditionObserver : MonoBehaviour {
+
+    public GameEvent finalTurnEndEvent;
+    public GameEvent coreDestroyedEvent;
+    public HeroEvent heroDeathEvent;
+
+    private RunTimePhaseData phaseData;
+
+    void Start() {
+
+        phaseData = GetComponent<RunTimePhaseData>();
+
+        heroDeathEvent.RegisterListener(HandleHeroDeathVerification);
+        finalTurnEndEvent.RegisterListener(HandleFinalTurnVerification);
+        coreDestroyedEvent.RegisterListener(HandleCoreDestroyedVerification);
+
+    }
+
+    private void HandleFinalTurnVerification() {
+
+        if (phaseData.idtPhaseType == PhaseObjectives.ATTACK) {
+
+            Debug.Log("Player não destruiu núcleo a tempo");
+
+        }
+        else if (phaseData.idtPhaseType == PhaseObjectives.DEFEND) {
+
+            Debug.Log("Player teve sucesso em defender núcleo");
+
+        }
+
+    }
+
+    private void HandleCoreDestroyedVerification() {
+
+        if (phaseData.idtPhaseType == PhaseObjectives.ATTACK) {
+
+            Debug.Log("Player teve sucesso em destruir o núcleo");
+
+        }
+        else if (phaseData.idtPhaseType == PhaseObjectives.DEFEND) {
+
+            Debug.Log("Player fracassou em defender o núcleo");
+
+        }
+
+    }
+
+    private void HandleHeroDeathVerification(RunTimeHeroData heroDead) {
+
+        if (phaseData.idtPhaseType == PhaseObjectives.ATTACK) {
+
+            if (!heroDead.isEnemy) {
+
+                Debug.Log("Player falhou ao comandar o ataque das tropas");
+
+            }
+
+        }
+        else if (phaseData.idtPhaseType == PhaseObjectives.DEFEND) {
+
+            if (heroDead.isEnemy) {
+
+                Debug.Log("Player teve sucesso em derrotar o comandante atacante");
+
+            }
+
+        }
+
+    }
+
+}
