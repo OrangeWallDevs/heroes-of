36bfe6f872b52accd815582cafd7e9661f96a12d
Daniel Franco Lopes <dandanielfranco@yahoo.com.br>
2019-10-12 20:49:40 -0300

    Add idtIsEnemy attribute to Tower entity class

--- Heroes Of/Assets/Scripts/Entity/Tower.cs
+++ Heroes Of/Assets/Scripts/Entity/Tower.cs
@@ -5,10 +5,12 @@ public class Tower : Entity {
     private int codTower;
     private int valHp;
     private int numEffectArea;
+    private bool idtIsEnemy;
 
     public int CodTower { get => codTower; set => codTower = value; }
     public int ValHp { get => valHp; set => valHp = value; }
     public int NumEffectArea { get => numEffectArea; set => numEffectArea = value; }
+    public bool IdtIsEnemy { get => idtIsEnemy; set => idtIsEnemy = value; }
 
     // Runtime members:
     public GameObject GameObject { get; set; }
