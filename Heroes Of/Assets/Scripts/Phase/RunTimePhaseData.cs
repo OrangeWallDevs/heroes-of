347ee05cf81372595ccfb9f6c1f61aa547443b39
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-10-11 21:06:13 -0300

    Create a RunTimeData for Phase

---
+++ Heroes Of/Assets/Scripts/Phase/RunTimePhaseData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunTimePhaseData : MonoBehaviour {
+
+    public int numPhase;
+    public int codPart;
+    public string namPhase;
+    public int valIniPlayerMoney;
+    public int valIniIAMoney;
+    public PhaseObjectives idtPhaseType;
+
+    public void SetData(Phase phaseData) {
+
+        numPhase = phaseData.NumPhase;
+        codPart = phaseData.CodPart;
+        namPhase = phaseData.NamPhase;
+        valIniPlayerMoney = phaseData.ValIniIAMoney;
+        valIniIAMoney = phaseData.ValIniIAMoney;
+        
+        switch (phaseData.IdtPhaseType) {
+
+            case ("ATTACK"):
+                idtPhaseType = PhaseObjectives.ATTACK;
+                break;
+
+            case ("DEFEND"):
+                idtPhaseType = PhaseObjectives.DEFEND;
+                break;
+
+        }
+
+    }
+
+}
