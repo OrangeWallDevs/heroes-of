57bafff5828572a7f0d04b7d7431dc5eae9dae85
Pedro <ppagbpedro@gmail.com>
2019-10-15 11:54:40 -0300

    Adapting available slot property name and location on feedback script

--- Heroes Of/Assets/Scripts/Feedback/AvaliableTileSelection.cs
+++ Heroes Of/Assets/Scripts/Feedback/AvaliableTileSelection.cs
@@ -8,10 +8,10 @@ public class AvaliableTileSelection : MonoBehaviour {
     public static void ChangeMask(TilemapHandler tilemapHandler, bool putMask){
 
 		foreach(KeyValuePair<Vector3Int, Node> pair in tilemapHandler.nodeTilemap.Nodes) {
-			if( pair.Value.tile.isSlot && pair.Value.tile.isSlotAvailable && putMask){
+			if( pair.Value.tile.isSlot && pair.Value.isAvailable && putMask){
 				pair.Value.tile.color = new Color(0f,56f,251f,1f);
 
-			}else if( pair.Value.tile.isSlot && pair.Value.tile.isSlotAvailable && !putMask){
+			}else if( pair.Value.tile.isSlot && pair.Value.isAvailable && !putMask){
 				pair.Value.tile.color = new Color(255f,255f,255f,255f);
 			}
 		}
