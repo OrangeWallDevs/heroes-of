2527fea5a9d37a55c7afa7257c6353fc09cf4de5
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-10-27 09:37:34 -0300

    Add scripts for testing the WinConditionObserver

---
+++ Heroes Of/Assets/Scripts/TestbuttonsScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestbuttonsScript : MonoBehaviour {
+
+    public void KillAllTroops() {
+
+        GameObject[] troops = GameObject.FindGameObjectsWithTag("Troop");
+
+        foreach (GameObject troop in troops) {
+
+            troop.SetActive(false);
+            Destroy(troop);
+
+        }
+
+    }
+
+    public void KillTheCore() {
+
+
+        GameObject core = GameObject.FindGameObjectWithTag("Core");
+
+        HealthController healthController = core.GetComponent<HealthController>();
+        healthController.ReceiveDamage((int) healthController.Health);
+
+    }
+
+    public void KillHero() {
+
+
+        GameObject core = GameObject.FindGameObjectWithTag("Hero");
+
+        HealthController healthController = core.GetComponent<HealthController>();
+        healthController.ReceiveDamage((int) healthController.Health);
+
+    }
+
+}
