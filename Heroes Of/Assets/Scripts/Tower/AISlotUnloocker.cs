16140c5f5f1ae9eeb0868c3277aa0d0096a40029
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-10-31 21:05:45 -0300

    Create script that unlock the Bob Enemy AI Slot

---
+++ Heroes Of/Assets/Scripts/Tower/AISlotUnloocker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AISlotUnloocker : MonoBehaviour {
+
+    public GameObject prefabAIBuildSlot;
+    public TowerEvent towerDestroyedEvent;
+
+    private void Start() {
+
+        towerDestroyedEvent.RegisterListener(HandleTowerDestroyed);
+
+    }
+
+    private void HandleTowerDestroyed(RunTimeTowerData towerDestroyed) {
+
+        Transform tower = gameObject.transform.parent;
+
+        if (tower == towerDestroyed.GameObject.transform) {
+
+            Instantiate(prefabAIBuildSlot, transform.position, Quaternion.identity);
+            towerDestroyedEvent.UnregisterListener(HandleTowerDestroyed);
+
+        }
+
+    }
+
+}
