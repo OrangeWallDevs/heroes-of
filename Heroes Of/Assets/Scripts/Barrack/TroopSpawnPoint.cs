1e5cb11e6fd3109f2c03517d4c9fbf96e3f74796
Gabriel Brandão <gabriel.silva.brandao7@gmail.com>
2019-11-09 20:16:16 -0300

    Create script to spawn troops on path tiles

---
+++ Heroes Of/Assets/Scripts/Barrack/TroopSpawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopSpawnPoint : MonoBehaviour {
+
+    private GameObject spawnPoint;
+    private TilemapHandler tilemapHandler;
+
+    void Start () {
+        tilemapHandler = GameObject.Find("TilemapHandler").GetComponent<TilemapHandler>();
+        spawnPoint = this.gameObject.transform.GetChild(0).gameObject;
+        Vector2 closestWalkablePosition = tilemapHandler.GetClosestWalkableTile(spawnPoint.transform.position);
+        spawnPoint.transform.position = closestWalkablePosition;
+    }
+}
--- Heroes Of/Assets/Scripts/Util/Tilemaps/TilemapHandler.cs
+++ Heroes Of/Assets/Scripts/Util/Tilemaps/TilemapHandler.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Tilemaps;
+using System.Linq;
+
 
 public class TilemapHandler : MonoBehaviour {
     public GameRuntimeData gameRuntimeData;
@@ -10,7 +12,6 @@ public class TilemapHandler : MonoBehaviour {
     public NodeTilemap nodeTilemap {get; private set;}
     private void Start() {
         nodeTilemap = new NodeTilemap(tilemap);
-        //tilemaps = gameRuntimeData.CurrentLevel.Tilemaps;
     }
 
     public bool IsTile(Vector2 position) {
@@ -19,14 +20,11 @@ public class TilemapHandler : MonoBehaviour {
 
     public Node GetTile(Vector3Int position) {
         return nodeTilemap.GetNode(position);
-        //return gameRuntimeData.NodeTilemap.GetNode(position);
     }
 
     public Vector3Int ScreenToCellPosition(Vector2 screenPos) {
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPos);
         Vector3Int cellPos = tilemap.WorldToCell(worldPosition);
-        // cellPos.x -= 1;
-        // cellPos.y -= 1;
         cellPos.z = 0;
         return cellPos;
     }
@@ -57,5 +55,17 @@ public class TilemapHandler : MonoBehaviour {
 
     public List<Node> GetAllTileNeighbours(Node node, int range) {
         return nodeTilemap.GetNeighbours(node, range);
+    }
+
+    public Vector2 GetClosestWalkableTile(Vector2 current) {
+        Vector3Int currentPosition = WorldToCellPosition(current);
+        Node currentNode = nodeTilemap.GetNode(currentPosition);
+
+        List<Node> neighbours = GetAllTileNeighbours(currentNode, 6);
+        neighbours = neighbours.Where(neighbour => neighbour.tile.isWalkable).ToList();
+        
+        Vector3Int closestWalkableNodePosition = neighbours[1].position;
+
+        return tilemap.CellToWorld(closestWalkableNodePosition);
     }
 } 
