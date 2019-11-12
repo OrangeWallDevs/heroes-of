e106ce42c4c3ef644018b7543eedad879541767a
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-12 01:24:30 -0300

    Creating script to display phase data to the user

---
+++ Heroes Of/Assets/Scripts/UI/LevelDetailsPanelController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDetailsPanelController : MonoBehaviour {
+
+    public Sprite idtAttackPhaseImage;
+    public Sprite idtDephensePhaseImage;
+    public TextMeshProUGUI phaseTitleLabel;
+
+    public TextMeshProUGUI userMaxScoreLabel;
+
+    public Image phaseTypeImage;
+    public GameRuntimeData gameRuntimeData;
+
+    void Start() {
+
+    }
+
+    public void ShowPhaseDetails(int phaseIndex) {
+        Phase selectedPhase = gameRuntimeData.Phases[phaseIndex - 1];
+
+        if (!(selectedPhase is null)) {
+            int userHighestScore = 0;
+
+            if(!(selectedPhase.UserScore is null)) 
+                userHighestScore = selectedPhase.UserScore.ValRecordPoints;
+
+            phaseTitleLabel.text = selectedPhase.NamPhase;
+            userMaxScoreLabel.text = userHighestScore.ToString();
+
+            if (selectedPhase.IdtPhaseType.ToLower() == "defense") {
+                phaseTypeImage.sprite = idtDephensePhaseImage;
+            } else {
+                phaseTypeImage.sprite = idtAttackPhaseImage;
+            }
+        }
+    }
+}
