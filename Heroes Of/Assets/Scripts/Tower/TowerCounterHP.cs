5c430a1b3dd227edf4248ea622694a2bdaa2c28c
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-11-11 22:22:29 -0300

    Create basic script to update the UI health counter over the tower

---
+++ Heroes Of/Assets/Scripts/Tower/TowerCounterHP.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TowerCounterHP : MonoBehaviour {
+
+    public TowerEvent towerBeingAttackedEvent;
+
+    private TextMeshProUGUI textCounterHP;
+    private RunTimeTowerData towerData;
+
+    private int startHp;
+
+    private void Awake() {
+
+        textCounterHP = GetComponent<TextMeshProUGUI>();
+        towerData = GetComponentInParent<RunTimeTowerData>();
+
+        startHp = towerData.valHp;
+
+    }
+
+    private void Start() {
+
+        towerBeingAttackedEvent.RegisterListener(UpdateHPCounter);
+
+        SetCounterValue(towerData.valHp);
+
+    }
+
+    private void OnDestroy() {
+
+        towerBeingAttackedEvent.UnregisterListener(UpdateHPCounter);
+
+    }
+
+    private void UpdateHPCounter(RunTimeTowerData towerAttacked) {
+
+        if (towerData.GameObject.Equals(towerAttacked.GameObject)) {
+
+            SetCounterValue(towerData.valHp);
+
+        }
+
+    }
+
+    private void SetCounterValue(int actualHp) {
+
+        textCounterHP.text = actualHp + "/" + startHp;
+
+    }
+
+}
