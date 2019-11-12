eb4b3939782485595f473ef377936152bea176e6
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-11-12 20:10:33 -0300

    Create a script to update the health_bar escale

---
+++ Heroes Of/Assets/Scripts/Feedback/HealthBar.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public Transform healthBar;
+    public FloatEvent healthChangedEvent;
+
+    private float maxHealth;
+
+    private void Start() {
+
+        HealthController healthController = GetComponentInParent<HealthController>();
+        maxHealth = healthController.Health;
+
+        healthChangedEvent.RegisterListener(UpdateHealthBar);
+
+    }
+
+    private void OnDestroy() {
+
+        healthChangedEvent.UnregisterListener(UpdateHealthBar);
+
+    }
+
+    private void UpdateHealthBar(float newHealthValue) {
+    
+        float healthPercentage = (newHealthValue / maxHealth);
+
+        SetSize(healthPercentage);
+
+    }
+
+    private void SetSize(float barSize) {
+
+        healthBar.localScale = new Vector3(barSize, 1f, 1f);
+
+    }
+
+}
