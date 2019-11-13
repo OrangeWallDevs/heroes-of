12feed5c60575125918604183ebca4e711f9e96d
Pedro Paulo <ppagbpedro@gmail.com>
2019-11-13 19:02:40 -0300

    Implementing script to play win or defeat sound when level ends

---
+++ Heroes Of/Assets/Scripts/Audio/GameEndSoundHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndSoundHandler : MonoBehaviour {
+    public AudioData victoryTheme;
+    public AudioData defeatTheme;
+
+    public GameEvent playerWinEvent;
+    public GameEvent playerLooseEvent;
+
+    void Start() {
+        playerWinEvent.RegisterListener(PlayVictorySong);
+        playerLooseEvent.RegisterListener(PlayDefeatSong);
+    }
+
+    public void PlayVictorySong() {
+        victoryTheme.Play();
+    }
+
+    public void PlayDefeatSong() {
+        defeatTheme.Play();
+    }
+}
