1931d5b04e1398864a56753875b4b9ad899516dc
Daniel Franco Lopes <dandanielfranco@yahoo.com.br>
2019-11-13 18:48:01 -0300

    Change asset loading directory to Resources

--- Heroes Of/Assets/Scripts/Util/Data IO/DataUtil.cs
+++ Heroes Of/Assets/Scripts/Util/Data IO/DataUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class DataUtil : Singleton<DataUtil> {
@@ -38,20 +39,35 @@ public class DataUtil : Singleton<DataUtil> {
 
     public List<T> LoadAssets<T>(string filter, string[] searchInFolders)
             where T : UnityEngine.Object {
-        // string[] results = searchInFolders != null ? AssetDatabase.FindAssets(filter, searchInFolders)
-        //                                            : AssetDatabase.FindAssets(filter);
+
         var assets = new List<T>();
 
-        // foreach (string result in results) {
-        //     string path = AssetDatabase.GUIDToAssetPath(result);
-        //     T asset = AssetDatabase.LoadAssetAtPath<T>(path);
-
-        //     if (asset != null) {
-        //         assets.Add(asset);
-        //     }
-        // }
+        searchInFolders = searchInFolders ?? new[] { "" };
+        Debug.Log($"searchInFolders: {searchInFolders}");
+        foreach (string folder in searchInFolders) {
+            foreach (T foundAsset in LoadAssetsHelper<T>(filter, folder)) {
+                assets.Add(foundAsset);
+            }                
+        }
 
         return assets;
+    }
+
+    IEnumerable<T> LoadAssetsHelper<T>(string filter, string folder) where T : UnityEngine.Object {
+        string path = $"{folder}{(folder == "" ? folder : "/")}{filter ?? ""}";
+        Object[] foundAssets = Resources.LoadAll(path);
+
+        foreach (Object foundAsset in foundAssets) {
+            if (!(foundAsset is null)) {
+                if (typeof(T) == typeof(GameObject)) {
+                    yield return foundAsset as T;
+                } else if (foundAsset is GameObject && typeof(Component).IsAssignableFrom(typeof(T))) {
+                    yield return (foundAsset as GameObject).GetComponent<T>();
+                } else {
+                    yield return null;
+                }
+            }
+        }
     }
 
 }
